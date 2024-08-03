dd93e438c5af3739bf9449976b38f98c71617861
Morsalin <morsalinmahmood@gmail.com>
2024-08-03 00:30:31 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var counter = 1;
+setInterval(function() {
+    document.getElementById('radio' + counter).checked = true;
+    counter++;
+    if (counter > 4) {
+        counter = 1;
+    }
+}, 4000);
+
+document.addEventListener('DOMContentLoaded', () => {
+    const faders = document.querySelectorAll('.fade');
+    const options = {
+        threshold: 0.5,
+        rootMargin: '0px 0px -50px 0px'
+    };
+    const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+        entries.forEach(entry => {
+            if (!entry.isIntersecting) {
+                return;
+            } else {
+                entry.target.classList.add('fade-in');
+                appearOnScroll.unobserve(entry.target);
+            }
+        });
+    }, options);
+    faders.forEach(fader => {
+        appearOnScroll.observe(fader);
+    });
+});
